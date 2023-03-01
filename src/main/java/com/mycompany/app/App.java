0f24ccb5e39010df89569574fa0e0792a8b74407
DevopsFarmer <121556299+DevopsFarmer@users.noreply.github.com>
2023-03-01 13:19:46 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -9,9 +9,6 @@ public class App
     public long add(long first, long second) {
         int a;
         int b;
-        int c;
-        int d;
-        int e;
         return first + second;
     }
 
