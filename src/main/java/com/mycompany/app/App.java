37cb137043b709bb761a82b9a4d5a209a91aff0d
DevopsFarmer <121556299+DevopsFarmer@users.noreply.github.com>
2024-03-31 21:49:56 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -19,7 +19,7 @@ public class App
 
     public long multiply(long first, long second) {
         int xyz;
-        int ABC;
+        int abc;
         int mnc;
         return first * second;
     }
